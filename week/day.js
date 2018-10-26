6043c49bc6ec34f2fc239444ad220b557e51a692
Maridmi <yarova0marina@gmail.com>
2018-10-26 21:33:42 +0300

    s

--- week/day.js
+++ week/day.js
@@ -1,12 +1,12 @@
 var day = new Date();
 var weekday = new Array(7);
-weekday[0] = "sunday";
-weekday[1] = "monday";
-weekday[2] = "tuesday";
-weekday[3] = "wednesday";
+weekday[0] = "Huh, right... It's sunday.";
+weekday[1] = "Monday??? Monday...";
+weekday[2] = "Hm, it's tuesday. I guess...";
+weekday[3] = "What a perfect day? It's wednesday!";
 weekday[4] = "thursday";
-weekday[5] = "friday";
-weekday[6] = "saturday";
+weekday[5] = "Yeeeah! Friday! I love it.";
+weekday[6] = "Saturday? OMG I hope so!";
 
 var enter = weekday[day.getDay()];
 document.getElementById("day").innerHTML = enter;
