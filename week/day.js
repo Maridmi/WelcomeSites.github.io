2ac9e6e2b330955bee4012a7ee1070ea45334e31
Mari <38698671+Maridmi@users.noreply.github.com>
2018-11-01 14:14:21 +0300

    Update day.js

--- week/day.js
+++ week/day.js
@@ -1,12 +1,18 @@
-var day = new Date();
-var weekday = new Array(7);
+let day = new Date();
+let weekday = new Array(7);
+let description = new Array(7);
 weekday[0] = "Huh, right... It's sunday.";
 weekday[1] = "Monday??? Monday...";
 weekday[2] = "Hm, it's tuesday. I guess...";
 weekday[3] = "What a perfect day? It's wednesday!";
-weekday[4] = "thursday";
+weekday[4] = "It's thursday, honey!";
 weekday[5] = "Yeeeah! Friday! I love it.";
-weekday[6] = "Saturday? OMG I hope so!";
+weekday[6] = "Saturday? My congratulations!";
 
-var enter = weekday[day.getDay()];
+description[4] = "Thursday is a good day."
+
+let enter = weekday[day.getDay()];
 document.getElementById("day").innerHTML = enter;
+
+let pass = description[day.getDay()];
+document.getElementById("description").innerHTML = pass;
